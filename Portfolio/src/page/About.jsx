0d6f7ed0e3bde5e7f81daaf69ae0480ad6997e7a
ZEnd <om99305@gmail.com>
2024-11-07 18:59:02 +0700

    add vercel

--- Portfolio/src/page/About.jsx
+++ Portfolio/src/page/About.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import { FaInstagram } from "react-icons/fa";
 const About = () => {
   return (
-    <div>
-      About
+    <div className="bg-black h-screen w-screen">
+      <div>
+        <div className="absolute bg-slate-600 h-[200px] w-[200px] rounded-xl m-5 justify-items-center">
+          <div className=" bg-red-400 w-[200px] h-[200px] rounded-xl items-center">
+            <FaInstagram color="white" size={42} />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
